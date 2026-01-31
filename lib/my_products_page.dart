e55dff35c6f0cc497f65448e92d3e37e74e8134b
Your Name <you@example.com>
2026-01-31 08:46:33 +0300

    inof

--- lib/my_products_page.dart
+++ lib/my_products_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:flutter/material.dart';
 import 'package:teftef/services/api_service.dart';
 import 'package:teftef/core/config.dart';
